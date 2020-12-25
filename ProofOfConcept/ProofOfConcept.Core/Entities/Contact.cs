8b9fd761c93e95b3d10c435515e26cc4b5d285b8
Fuji Nguyen <fuji.nguyen@workcontrol.com>
2020-12-24 19:57:59 -0500

    add Incident and Contact models to CORE

---
+++ ProofOfConcept/ProofOfConcept.Core/Entities/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProofOfConcept.Core.Entities
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ ProofOfConcept/ProofOfConcept.Core/Entities/Incident.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace ProofOfConcept.Core.Entities
+{
+    public class Incident
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTime ReportedOn { get; set; }
+        public Contact ReportedBy { get; set; }
+    }
+}
