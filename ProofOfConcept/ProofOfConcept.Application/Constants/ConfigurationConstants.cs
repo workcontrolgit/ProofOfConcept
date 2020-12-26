b63a103668ecfdd8f8a2db405b990bb29de4b0cd
fuji.nguyen <fuji.nguyen@workcontrol.com>
2020-12-26 11:41:33 -0500

    update readme.md

--- ProofOfConcept/ProofOfConcept.Application/Constants/ConfigurationConstants.cs
+++ ProofOfConcept/ProofOfConcept.Application/Constants/ConfigurationConstants.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ProofOfConcept.Application.Constants
+﻿namespace ProofOfConcept.Application.Constants
 {
     public static class ConfigurationConstants
     {
--- ProofOfConcept/ProofOfConcept.Application/Parameters/GetAllProductsParameter.cs
+++ ProofOfConcept/ProofOfConcept.Application/Parameters/GetAllProductsParameter.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ProofOfConcept.Application.Parameters
+﻿namespace ProofOfConcept.Application.Parameters
 {
     public class GetAllProductsParameter : RequestParameter
     {
